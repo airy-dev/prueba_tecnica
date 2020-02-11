07509e4490c5b8dbc2da32ea3aa7d87b4afc9ebb
AIRY <tomy.007.mk@gmail.com>
2020-02-10 20:36:12 -0600

    commit con el proyecto de ejemplo vue firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = ''
+
+firebase.initializeApp ({
+  apiKey: "AIzaSyBzbpaY992WAite5cSgK4AuKu3K48-Tpd4",
+  authDomain: "vue-firebase-tutorial-5c478.firebaseapp.com",
+  databaseURL: "https://vue-firebase-tutorial-5c478.firebaseio.com",
+  projectId: "vue-firebase-tutorial-5c478",
+  storageBucket: "vue-firebase-tutorial-5c478.appspot.com",
+  messagingSenderId: "836418149293",
+  appId: "1:836418149293:web:b9554af817dfed7cd81f24"
+});
+
+firebase.auth().onAuthStateChanged(() => {
+	if (!app) {
+		app = new Vue({
+			router,
+			render: h => h(App)
+		}).$mount('#app')
+	}
+});
